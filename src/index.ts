3135d55c6e41b364d5a72a52ee86842d30d6d251
Exan <exan@landviz.nl>
2019-11-10 11:23:48 +0100

    Wtf is wrong with this vps

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
-import { Bot } from "./Bot";
-import { HtmlJson } from "./HtmlJson";
+// import { Bot } from "./Bot";
+// import { HtmlJson } from "./HtmlJson";
 
-require('dotenv').config();
+// require('dotenv').config();
 
-let test = new HtmlJson('https://cubecraft.net');
-test.loadDom();
+// let test = new HtmlJson('https://cubecraft.net');
+// test.loadDom();
 
 // let bot = new Bot(process.env.TOKEN, Number(process.env.REFRESHTIMER));
+import * as puppeteer from 'puppeteer';
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    await page.goto(this.url);
+    
+    await page.waitFor(7500);
+
+    console.log(await page.evaluate(() => {
+        return document.documentElement.outerHTML;
+    }));
+})()
